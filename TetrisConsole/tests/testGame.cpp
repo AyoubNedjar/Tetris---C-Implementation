708e413bec879bc55bc6e02d563b38f674e75962
58434 <58434@etu.he2b.be>
2024-04-05 17:53:31 +0200

    Ajout test

--- TetrisConsole/tests/testGame.cpp
+++ TetrisConsole/tests/testGame.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/Model/Game.h"
+#include "../src/directoryBrick/square.h"
 
 TEST_CASE( "Test of Game") {
 
@@ -9,10 +10,17 @@ TEST_CASE( "Test of Game") {
         int initialNbLigneComplete = game.getNbLigneComplete();
         game.moveBrick(Direction::DOWN, false);
         REQUIRE(game.getScore() == initialScore);
-        REQUIRE(game.getNbLigneComplete() == initialNbLigneComplete);
+        REQUIRE(game.getNbLigneComplete() == initialNbLigneComplete); // The score didn't change because it's a move not a drop .
     }
-
     SECTION("Test drop method"){
-
+        Game game;
+        //Default board setting .
+        //Height == 20 .
+        //Width == 15 .
+        std::unique_ptr<Brick> currentBrick = std::make_unique<Square>();
+        std::vector<Position> listOfCurrentPositions = currentBrick->getPositionsTrue();
+        game.getScore();
+        game.drop();
+        REQUIRE(game.getScore() == (40 * 0 + game.getBoard().getHeight()-2)* 1); // getHeight-2 because height of square is 2 .
     }
 }
